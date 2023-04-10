0fd8c4e8f1859fd88fd21e5d0f038ed961f85181
Miranda Lieblein <miranda.lieblein@gmail.com>
2023-04-10 17:19:31 -0400

    Changed directions and tags to varchar in DB

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -29,8 +29,8 @@ CREATE TABLE users_ingredients (
 CREATE TABLE recipes(
 	recipe_id serial,
 	recipe_name varchar(50) NOT NULL,
-	directions text[] NOT NULL,
-	tags text[],
+	directions varchar(1500) NOT NULL,
+	tags varchar(300),
 	prep_time int,
 	food_pic varchar(100),
 	is_public boolean,
@@ -101,21 +101,26 @@ CREATE TABLE recipe_ingredients(
 );
 
 INSERT INTO recipes(recipe_name,directions,tags,prep_time,food_pic,is_public) 
-VALUES('Crock Pot Roast',ARRAY['Place beef roast in crock pot.',
-                'Mix the dried mixes together in a bowl and sprinkle over the roast.',
-                'Pour the water around the roast.',
-                'Cook on low for 7-9 hours.'],ARRAY['gluten free'],10,null,true);
+VALUES('Crock Pot Roast','Place beef roast in crock pot.  Mix the dried mixes together in a bowl and sprinkle over the roast. Pour the water around the roast. 
+	   Cook on low for 7-9 hours.','gluten-free',10,null,true);
 
 INSERT INTO recipes(recipe_name,directions,tags,prep_time,food_pic,is_public) 
-VALUES('Roasted Asparagus',ARRAY[ 'Preheat oven to 425°F.',
-                'Cut off the woody bottom part of the asparagus spears and discard.',
-                'With a vegetable peeler, peel off the skin on the bottom 2-3 inches of the spears (this keeps the asparagus from being all., and if you eat asparagus you know what I mean by that).',
-                'Place asparagus on foil-lined baking sheet and drizzle with olive oil.',
-                'Sprinkle with salt.',
-                'With your hands, roll the asparagus around until they are evenly coated with oil and salt.',
-                'Roast for 10-15 minutes, depending on the thickness of your stalks and how tender you like them.',
-                'They should be tender when pierced with the tip of a knife.',
-                'The tips of the spears will get very brown but watch them to prevent burning.',
-                'They are great plain, but sometimes I serve them with a light vinaigrette if we need something acidic to balance out our meal.'],ARRAY['Healthy'],20,null,true);
+VALUES('Roasted Asparagus','Preheat oven to 425°F.'
+	   'Cut off the woody bottom part of the asparagus spears and discard.
+	   With a vegetable peeler, peel off the skin on the bottom 2-3 inches of the spears (this keeps the asparagus from being all., and if you eat asparagus you know what I mean by that).
+	   Place asparagus on foil-lined baking sheet and drizzle with olive oil.
+	   Sprinkle with salt.
+	   With your hands, roll the asparagus around until they are evenly coated with oil and salt.
+	   Roast for 10-15 minutes, depending on the thickness of your stalks and how tender you like them.
+	   They should be tender when pierced with the tip of a knife.
+	   The tips of the spears will get very brown but watch them to prevent burning.
+	   They are great plain, but sometimes I serve them with a light vinaigrette if we need something acidic to balance out our meal.','Healthy',20,null,true);
+	   
+	   
+
+INSERT INTO users_recipes (user_id, recipe_id)
+VALUES (1, 1);
+INSERT INTO users_recipes (user_id, recipe_id)
+VALUES (2, 2);
 
 COMMIT TRANSACTION;
