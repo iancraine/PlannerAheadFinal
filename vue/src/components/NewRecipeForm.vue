cddddd84a87455b4a1e895f1048f8501db1b1b04
Miranda Lieblein <miranda.lieblein@gmail.com>
2023-04-11 16:42:01 -0400

    form changes

--- vue/src/components/NewRecipeForm.vue
+++ vue/src/components/NewRecipeForm.vue
@@ -3,22 +3,31 @@
         <form action="#">
             <label for="recipe-name">Recipe Name: </label>
             <input type="text" class="recipe-name" id="recipe-name" />
-
-
-        <div class="ingredientsDropdown">
             
-            <div id="myDropdown" class="ingredients-content">
+            <div  class="ingredients-content">
                 <label for="userInput">Add Ingredients: </label>
-                <input type="text" placeholder="Search.." onkeyup="filterFunction()" id="userInput">
-                <div v-for="ingredient in ingredients" v-bind:key="ingredient.id">
-                    <h5>{{ingredient.name}}</h5>
-                    
-                </div>
-
+                <input type="text" id="userInput">
+                <label for="amount">Amount: </label>
+                <input type="text" id="amount">
+                <label for="unit">Unit: </label>
+                <select name="units" id="units">
+                    <option value="cups">cups</option>
+                    <option value="Tbsp">Tbsp</option>
+                    <option value="Tsp">Tsp</option>
+                    <option value="grams">g</option>
+                    <option value="pounds">lbs</option>
+                    <option value="ounces">oz</option>
+                    <option value="quarts">quarts</option>
+                    <option value="pints">pints</option>
+                    <option value="gallons">gallons</option>
+                </select>
                 <button class="addIngredient">Add</button>
             </div>
-        </div>
-           
+            <div>
+                <ul v-for="ingredient in ingredients" v-bind:key="ingredient">
+                    <li>{{ingredient.name}} {{ingredient.amount}}</li>
+                </ul>
+            </div>
         </form>
     </div>
 </template>
@@ -36,7 +45,13 @@ export default {
            prep_time:'',
            food_pic:'',
            is_public:''
-        }
+        },
+        ingredients: [
+            {
+                name:'',
+                amount: '',
+            }
+        ]
     };
 },
 
@@ -47,7 +62,16 @@ export default {
 .addIngredient {
     background-color: #AFE1AF;
     color: white;
-    padding: 
+    
+}
+li {
+    display: none;
+}
+.ingredients-content > input{
+    margin-right: 25px;
+}
+form > *{
+    margin-bottom: 15px;
 }
 
 </style>
