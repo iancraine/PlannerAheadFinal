a5ed5ef041020ab0f9f088d32f9d2cd8b7468c3b
Miranda Lieblein <miranda.lieblein@gmail.com>
2023-04-11 16:06:43 -0400

    fixing merge error

--- vue/src/components/NewRecipeForm.vue
+++ vue/src/components/NewRecipeForm.vue
@@ -4,9 +4,21 @@
             <label for="recipe-name">Recipe Name: </label>
             <input type="text" class="recipe-name" id="recipe-name" />
 
-            <label for="ingredients-search">Search Ingredients</label>
-            <input type="text" class="ingredients-search" id="ingredients-search" />
-            <button>Add</button>
+
+        <div class="ingredientsDropdown">
+            
+            <div id="myDropdown" class="ingredients-content">
+                <label for="userInput">Add Ingredients: </label>
+                <input type="text" placeholder="Search.." onkeyup="filterFunction()" id="userInput">
+                <div v-for="ingredient in ingredients" v-bind:key="ingredient.id">
+                    <h5>{{ingredient.name}}</h5>
+                    
+                </div>
+
+                <button class="addIngredient">Add</button>
+            </div>
+        </div>
+           
         </form>
     </div>
 </template>
@@ -32,5 +44,10 @@ export default {
 </script>
 
 <style>
+.addIngredient {
+    background-color: #AFE1AF;
+    color: white;
+    padding: 
+}
 
 </style>
