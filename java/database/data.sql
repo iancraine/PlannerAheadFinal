2e81d16809daa8b73a5ab178323ac0fdf9f98a5b
Miranda Lieblein <miranda.lieblein@gmail.com>
2023-04-11 12:20:56 -0400

    Recipe Endpoints tested and fixed

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,10 +3,10 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
-INSERT INTO recipes(recipe_name,directions,tags,prep_time,food_pic,is_public) 
-VALUES('Crock Pot Roast','Place beef roast in crock pot.  Mix the dried mixes together in a bowl and sprinkle over the roast. Pour the water around the roast. Cook on low for 7-9 hours.','gluten-free',10,null,true);
+INSERT INTO recipes(recipe_name,directions,tags,prep_time,food_pic,is_public, created_by) 
+VALUES('Crock Pot Roast','Place beef roast in crock pot.  Mix the dried mixes together in a bowl and sprinkle over the roast. Pour the water around the roast. Cook on low for 7-9 hours.','gluten-free',10,null,true,1);
 
-INSERT INTO recipes(recipe_name,directions,tags,prep_time,food_pic,is_public) 
+INSERT INTO recipes(recipe_name,directions,tags,prep_time,food_pic,is_public,created_by) 
 VALUES('Roasted Asparagus','Preheat oven to 425°F.' 
 	   'Cut off the woody bottom part of the asparagus spears and discard.' 
 	   'With a vegetable peeler, peel off the skin on the bottom 2-3 inches of the spears' 
@@ -16,7 +16,7 @@ VALUES('Roasted Asparagus','Preheat oven to 425°F.'
 	   Roast for 10-15 minutes, depending on the thickness of your stalks and how tender you like them.
 	   They should be tender when pierced with the tip of a knife.
 	   The tips of the spears will get very brown but watch them to prevent burning.
-	   They are great plain, but sometimes I serve them with a light vinaigrette if we need something acidic to balance out our meal.','Healthy',20,null,true);
+	   They are great plain, but sometimes I serve them with a light vinaigrette if we need something acidic to balance out our meal.','Healthy',20,null,true,2);
 
 INSERT INTO users_recipes (user_id, recipe_id) VALUES (1,1);
 
