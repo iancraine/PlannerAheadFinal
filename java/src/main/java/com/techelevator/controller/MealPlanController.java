7b264b09d83c2a2d03c0a26e7f0a02f5f0601541
Nadia Shahid <nadiatx2015@gmail.com>
2023-04-13 11:35:19 -0500

    Meal Plan Controller

--- java/src/main/java/com/techelevator/controller/MealPlanController.java
+++ java/src/main/java/com/techelevator/controller/MealPlanController.java
@@ -1,19 +1,46 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.MealPlanDao;
+import com.techelevator.model.MealPlan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.security.auth.message.callback.SecretKeyCallback;
+import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/mealplans/")
+@RequestMapping("/mealplans")
 public class MealPlanController {
 
     @Autowired
     private MealPlanDao mealPlanDao;
 
-    
+    @RequestMapping(path="/{userid}",method= RequestMethod.GET)
+    public List<MealPlan> listAllMealPlans(@PathVariable int userId) {
+
+
+       return mealPlanDao.listAllMealPlans (userId);
+    }
+
+    @RequestMapping(path="/{mealPlanId}", method= RequestMethod.GET)
+    public MealPlan getMealPlanById(@PathVariable int mealPlanId){
+        return mealPlanDao.getMealPlanById(mealPlanId);
+    }
+
+ @RequestMapping(path="/add/{userId}", method= RequestMethod.POST)
+   public  MealPlan addMealPlan(@PathVariable int userId, @RequestBody MealPlan mealPlan){
+        return mealPlanDao.addMealPlan(mealPlan, userId);
+ }
+
+ @RequestMapping(path="/{userId}/{mealPlanId}", method=RequestMethod.DELETE)
+    public void deleteMealPlan(@PathVariable int mealPlanId, @PathVariable int userId){
+        mealPlanDao.deleteMealPlan(userId,mealPlanId);
+ }
+
+ @RequestMapping(path="/{mealPlanId}", method=RequestMethod.PUT)
+ public MealPlan updateMealPlan(@PathVariable int mealPlanId,@RequestBody MealPlan mealPlan){
+        return mealPlanDao.updateMealPlan(mealPlan);
+ }
 
 }
