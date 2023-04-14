b2c26ff9d8cc812c458365f964569a58c163dad6
Ian Craine <iancraine09@gmail.com>
2023-04-14 14:57:52 -0500

    up

--- vue/src/components/MealPlanList.vue
+++ vue/src/components/MealPlanList.vue
@@ -1,42 +1,29 @@
 <template>
   <div>
     <h1>My MealPlans</h1>
-
     <div v-for="mealplan in mealPlans" v-bind:key="mealplan">
-      <div>
-       <ul>
-        <li v-for="(day, index) in mealplan" v-bind:key="index">
-          <h2 v-if="index === 0">{{day.plan_name}}</h2>
-          <div v-else>
-            <div >
-            <tr>
-              <th>Meal Type</th>
-              <th>Recipe Id</th>
-              <th>Date</th>
-              </tr>
-            <tr v-for="day in mealplan" v-bind:key="day">
-              <td>{{day.meal_type}}</td>
-              <td>{{day.recipe_id}}</td>
-              <td>{{day.for_date}}</td>
-            </tr>
-            </div>
-            </div>
-        </li>
-       </ul>
-     </div>
-
-     
-
-
-
+      <h2>{{ mealplan[0].plan_name }}</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Meal Type</th>
+            <th>Recipe Id</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(day, index) in mealplan" v-bind:key="index">
+            <td>{{day.meal_type}}</td>
+            <td>{{day.recipe_id}}</td>
+            <td>{{day.for_date}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    
   </div>
 </template>
-
 <script>
 import mealPlanService from "../services/MealPlanService.js";
-
 export default {
     name: 'my-meal-plans',
     data() {
@@ -44,18 +31,12 @@ export default {
         mealPlans: []
       }
       },
-
     created() {
     mealPlanService.listAllMealPlans(this.$route.params.userId).then(response => {
       this.mealPlans = response.data;
-      
     });
   }
-
-
 }
 </script>
-
 <style>
-
 </style>
