96e941276bdb557adc488559163631f4214e00eb
Ian <iancraine09@gmail.com>
2023-09-30 16:36:32 -0500

    auth base url

--- vue/src/services/AuthService.js
+++ vue/src/services/AuthService.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
+const http = axios.create({
+  baseURL: "https://plannerahead-eeec72fe62b2.herokuapp.com"
+});
+
 export default {
 
   login(user) {
-    return axios.post('/login', user)
+    return http.post('/login', user)
   },
 
   register(user) {
-    return axios.post('/register', user)
+    return http.post('/register', user)
   },
 
 }
